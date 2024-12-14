245d96727262401f9c9fb5a2a151c2bdc98e06cc
gospod1spas1 <outofbrain72@gmail.com>
2024-12-14 16:30:13 +0500

    added forgotten reposiroties

--- src/Repositories/Interfaces/CommentsRepositoryInterface.php
+++ src/Repositories/Interfaces/CommentsRepositoryInterface.php
@@ -1,9 +1,12 @@
 <?php
 
+namespace App\Repositories\Interfaces;
+
 use App\Models\Comment;
 
 interface CommentsRepositoryInterface
 {
     public function get(string $uuid): ?Comment;
+
     public function save(Comment $comment): void;
 }
